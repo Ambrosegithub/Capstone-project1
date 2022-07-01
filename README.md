ae3ac9f6a317a797423e9466fafa6793f0f3ff88
Isigba Ambrose <ambrisexyz@gmail.com>
2022-07-01 03:12:25 +0200

    set up tools, add html files for both home and about page, add css files for both home and about page and script file.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
+## Project Name
+"My Capstone" is a project design in quest of 'Microverse course outline' for easy scalebility, accessibity, users friendly etc. so as to navigate through the web without any stress. Best tools and technologies was in high consideration during the ideal nuturing stage, debuging tools check was used('LINTERS') from the planning stage to implemntation stage, so your free and safe ride is guaranty.
+It is set up as a requirement for Microverse Course to sastisfy both your needs and our needs.
+## link interaction: 
+when click on nav menu it will take you to differnt destination and sections
+-- About leads to about page 
+-- Nav logo leads to home page sections.
+-- News leads to news sections.
+-- Sponsors leads to partners sections.
+-- program leads to event section.
+--join leads feature speaker section.
+--logo in the about page  takes you back to home page sections.
+## Live Demo 
+[Link to the live demo] (https://ambrosegithub.github.io/My-porfolio/)
+
+## Built With
+The major used for this project are : 
+HTML,
+CSS.
+linters
+github
+javascript
+
+## Technologies used
+The amazing tecnologies for this project are:
+Linters,
+git
+ ## How to  make a copy of this project(clone):
+ Visit my github url listed below
+ search project(repo name)
+ use git fork and pull to ur locall machine or download the zip code .. 
+
+## Author:
+
+- ## ISIGBA AMBROSE
+ ## Social:
+ Github:[@ambrosegithub](https://github.com/Ambrosegithub)
+-Twitter:[@Glorious12](https:mobile.twitter.com/Glorious851)
+ Facebook:Isigba Ambrose Davaid
+ Email: isigba.ammbrose94@gmail.com
+ Whatsapp tel: +393512463798
+ Contact: +3938817911630, +393298582707.
+ 
 # Capstone-project1
---
+++ script.js
@@ -0,0 +1,136 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-links');
+hamburger.addEventListener('click', () => {
+  // alert("i am here");
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
+
+const projectContainerArr = [
+    {
+      headline: 'Feature Speakers',
+      image: './images/provost.jpg',
+      name: ' Prof DR Lukeman',
+      intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Iste.',
+      topic: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis molestias consequuntur quod qui quisquam voluptatibusmaxime voluptatum harum aperiam dolores.',
+    },
+    {
+      headline: 'Feature Speakers',
+      image: './WhatsApp.img5.jpeg',
+      name: 'Snitarian Godswill',
+      intro: 'REHO, LEHO. HND ENV HEALTH.',
+      topic: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis molestias consequuntur quod qui quisquam voluptatibusmaxime voluptatum harum aperiam dolores.',
+    },
+    {
+      headline: 'Feature Speakers',
+      image: './images/photo2.jpg',
+      name: 'PROF Dr(Mrs) Ken Joyce',
+      intro: 'Msc in public Health Nurse.',
+      topic: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis molestias consequuntur quod qui quisquam voluptatibusmaxime voluptatum harum aperiam dolores.',
+    },
+    {
+      headline: 'Feature Speakers',
+      image: './images/photo5.jpg',
+      name: 'PROF (Mrs) Osarienwmen Osasere',
+      intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Iste.',
+      topic: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis molestias consequuntur quod qui quisquam voluptatibusmaxime voluptatum harum aperiam dolores.',
+    },
+    {
+      headline: 'Feature Speakers',
+      image: './images/photo4.jpg',
+      name: 'DR Lukeman',
+      intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Iste.',
+      topic: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis molestias consequuntur quod qui quisquam voluptatibusmaxime voluptatum harum aperiam dolores.',
+    },
+    {
+      headline: 'Feature Speakers',
+      image: './images/photo3.jpg',
+      name: 'DR Lukeman',
+      intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Iste.',
+      topic: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Veritatis molestias consequuntur quod qui quisquam voluptatibusmaxime voluptatum harum aperiam dolores.',
+    },
+  ];
+  const featureSection = document.querySelector('.feature-speaker');
+   for(let i = 0; i < projectContainerArr.length; i += 1){
+   
+    featureSection.innerHTML = `
+<h3 class="featureheadline">${projectContainerArr[0].headline}</h3>
+        <div class="feature-speaker-container">
+          <div class="speaker1">
+            <img id="profile-img1" src= "${projectContainerArr[0].image}"alt="profile-image" />
+            <div id="speaker-profile1">
+              <h4>${projectContainerArr[0].name}</h4>
+              <small>${projectContainerArr[0].intro}</small>
+              <p>${projectContainerArr[0].topic}</p>
+            </div>
+          </div>
+          <div class="speaker2">
+            <img id="profile-img2" src="${projectContainerArr[1].image}"alt="profile-image" />
+            <div id="speaker-profile2">
+            <h4>${projectContainerArr[1].name}</h4>
+            <small>${projectContainerArr[1].intro}</small>
+            <p>${projectContainerArr[1].topic}</p>
+            </div>
+          </div>
+          <div class="speaker3 view-more">
+          <img id="profile-img2" src="${projectContainerArr[2].image}"alt="profile-image" />
+          <div id="speaker-profile2">
+          <h4>${projectContainerArr[2].name}</h4>
+          <small>${projectContainerArr[2].intro}</small>
+          <p>${projectContainerArr[2].topic}</p>
+          </div>
+          </div>
+          <div class="speaker4 view-more">
+            <img id="profile-img2" src="${projectContainerArr[3].image}"alt="profile-image" />
+            <div id="speaker-profile2">
+            <h4>${projectContainerArr[3].name}</h4>
+            <small>${projectContainerArr[3].intro}</small>
+            <p>${projectContainerArr[3].topic}</p>
+            </div>
+          </div>
+          <div class="speaker5 view-more">
+          <img id="profile-img2" src="${projectContainerArr[4].image}"alt="profile-image" />
+          <div id="speaker-profile2">
+          <h4>${projectContainerArr[4].name}</h4>
+          <small>${projectContainerArr[4].intro}</small>
+          <p>${projectContainerArr[4].topic}</p>
+          </div>
+          </div>
+          <div class="speaker6 view-more">
+          <img id="profile-img2" src="${projectContainerArr[5].image}"alt="profile-image" />
+          <div id="speaker-profile2">
+          <h4>${projectContainerArr[5].name}</h4>
+          <small>${projectContainerArr[5].intro}</small>
+          <p>${projectContainerArr[5].topic}</p>
+          </div>
+          </div>
+        </div>
+        <button id="see-more-btn" type="button">More<span id="see-more" class="material-symbols-outlined">
+            expand_more
+        </span></button>
+        `;
+}
+
+// const openPages = document.querySelector('#see-more-btn');
+//    const buttons = document.getElementsByClassName('view-more');
+//    buttons.classList.add('.view-more')
+//     openPages.addEventListener('click', () => {
+//       // alert("i am here")
+//     buttons.style.display = 'block';
+// });
+
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//       document.getElementsByClassName('view-more').style.display = 'block';
+  
+//     })
+//   });
+//   closeModal.addEventListener('click', () => {
+//     document.getElementById('my-modal').style.display = 'none';
+//   });
